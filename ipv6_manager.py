ae2cb59ca69c77db20de8511ad8ac59f91b6b729
Carol-Nunes <cacanunesaraujo@gmail.com>
2026-06-12 15:11:19 -0300

    Implement IPv6 address validation

--- ipv6_manager.py
+++ ipv6_manager.py
@@ -130,9 +130,96 @@ def format_ipv6(ipv6_blocks):
 
     return formatted_address
 
+'''
+Valida um endereço IPv6 no formato IPv6/CIDR.
+A função verifica:
+Se o endereço está no formato correto (IPv6/CIDR);
+Se o prefixo está entre 0 e 128;
+Se existe no máximo uma ocorrência de "::";
+Se a quantidade de blocos IPv6 é válida;
+cada bloco possui no máximo 4 dígitos hexadecimais;
+Se todos os caracteres pertencem ao conjunto hexadecimal.
+Retorna True caso o endereço seja válido e False caso contrário.
+'''
 
-def validate_ipv6(ip):
-    pass
+def validate_ipv6(ipv6_block):
+
+    try:
+
+        ipv6_address, prefix_length = ipv6_block.split('/')
+
+        prefix_length = int(prefix_length)
+
+    except:
+
+        return False
+
+    # Verifica se o prefixo está entre 0 e 128.
+    if prefix_length < 0 or prefix_length > 128:
+
+        return False
+    
+    # Verifica se existe mais de um "::".
+    if ipv6_address.count('::') > 1:
+
+        return False 
+    
+    # Se existir "::", contamos os blocos dos dois lados.
+    if '::' in ipv6_address:
+
+        left_part, right_part = ipv6_address.split('::')
+
+        left_blocks = []
+
+        for block in left_part.split(':'):
+
+            if block: 
+
+                left_blocks.append(block)
+
+        right_blocks = []
+
+        for block in right_part.split(':'):
+
+            if block:
+
+                right_blocks.append(block)
+
+        left_blocks_length = len(left_blocks)
+        right_blocks_length = len(right_blocks)
+
+        if(left_blocks_length + right_blocks_length) >= 8:
+
+            return False
+        
+        blocks = left_blocks + right_blocks
+
+    else:
+
+        blocks = ipv6_address.split(':')
+
+        blocks_length = len(blocks)
+
+        if blocks_length != 8:
+
+            return False
+        
+    # Verifica tamanho dos blocos e caracteres hexadecimais.
+    valid_chars = "0123456789abcdefABCDEF"
+
+    for block in blocks:
+
+        if len(block) > 4:
+
+            return False
+        
+        for char in block:
+
+            if char not in valid_chars:
+
+                return False
+            
+    return True
 
 def generate_locations():
     pass
